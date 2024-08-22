4ae11b5a81f340e2194f68ba372004aa8f9f137b
Zach Podbielniak <z.podbielniak@disroot.org>
2024-08-22 03:43:51 -0400

    feat: added support for multiple custom packages yamls (for downstream layering)

--- src/packages.sh
+++ src/packages.sh
@@ -3,25 +3,17 @@
 # but we are expecting to run it from the Makefile
 # so it can be relative to it
 PACKAGES_FILE="./packages.yaml"
+# Custom Pattern
+PACKAGES_CUSTOM_FMT="./packages.custom-*.yaml"
+source ./src/common.sh
 
 
-make_export () {
-    [ "$#" != 1 ] && echo "$0 <bin>" && exit 1
-    mkdir -p ~/bin/export
-    [ -f "~/bin/export/$1" ] && rm "~/bin/export/$1" 
-    distrobox-export --bin $(which "$1") --export-path ~/bin/export/
-}
-
-
-make_app () {
-    [ "$#" != 1 ] && echo "$0 <app>" && exit 1
-    distrobox-export --app "$1"
-}
-
-
+# Arg 1 is path to packages.yaml
 get_yaml_distrobox_length() {
+    [ $# -ne 1 ] && echo "$0 <packages.yaml>" && exit 1
+    local packages_yaml="$1"
     local key=".distrobox[].name"
-    local length=$(yq "${key}" < $PACKAGES_FILE | wc -l)
+    local length=$(yq "${key}" < $packages_yaml | wc -l)
     echo $length
 }
 
@@ -29,21 +21,22 @@ get_yaml_distrobox_length() {
 # Arg should be index
 dbox_install_single() {
     [ ! -f /run/.containerenv ] && echo "This is not a container!" && exit 1
-    [ $# -ne 1 ] && echo "$0 <index>" && exit 1
+    [ $# -ne 2 ] && echo "$0 <packages.yaml> <index>" && exit 1
 
-    local index="$1"
+    local packages_yaml="$1"
+    local index="$2"
     local key=".distrobox[${index}]"
-    local name=$(yq "${key}.name" < $PACKAGES_FILE)
-    local image=$(yq "${key}.image" < $PACKAGES_FILE)
-    local pkg_inst_cmd=$(yq "${key}.pkg_inst_cmd" < $PACKAGES_FILE)
-    local pkg_updt_cmd=$(yq "${key}.pkg_updt_cmd" < $PACKAGES_FILE)
-    local extra_commands=$(yq "${key}.extra_commands" < $PACKAGES_FILE)
-    local packages=$(yq "${key}.packages[]" < $PACKAGES_FILE)
-    local npm_packages=$(yq "${key}.npm_packages[]" < $PACKAGES_FILE)
-    local pip_packages=$(yq "${key}.pip_packages[]" < $PACKAGES_FILE)
-    local bin_export=$(yq "${key}.bin_export[]" < $PACKAGES_FILE)
-    local app_export=$(yq "${key}.app_export[]" < $PACKAGES_FILE)
-    local bin_symlink=$(yq "${key}.bin_symlink[]" < $PACKAGES_FILE)
+    local name=$(yq "${key}.name" < $packages_yaml)
+    local image=$(yq "${key}.image" < $packages_yaml)
+    local pkg_inst_cmd=$(yq "${key}.pkg_inst_cmd" < $packages_yaml)
+    local pkg_updt_cmd=$(yq "${key}.pkg_updt_cmd" < $packages_yaml)
+    local extra_commands=$(yq "${key}.extra_commands" < $packages_yaml)
+    local packages=$(yq "${key}.packages[]" < $packages_yaml)
+    local npm_packages=$(yq "${key}.npm_packages[]" < $packages_yaml)
+    local pip_packages=$(yq "${key}.pip_packages[]" < $packages_yaml)
+    local bin_export=$(yq "${key}.bin_export[]" < $packages_yaml)
+    local app_export=$(yq "${key}.app_export[]" < $packages_yaml)
+    local bin_symlink=$(yq "${key}.bin_symlink[]" < $packages_yaml)
 
 
     bash -c "$extra_commands"
@@ -55,13 +48,13 @@ dbox_install_single() {
     type npm 2>/dev/null
     if [ 0 -eq $? ]
     then 
-        sudo npm i -g $(for pkg in $npm_packages; do printf ' %s' $pkg; done)
+        [ "" != "$npm_packages" ] && sudo npm i -g $(for pkg in $npm_packages; do printf ' %s' $pkg; done)
     fi 
     
     type pip3 2>/dev/null
     if [ 0 -eq $? ]
     then 
-        sudo pip3 install $(for pkg in $pip_packages; do printf ' %s' $pkg; done)
+        [ "" != "$pip_packages" ] && sudo pip3 install $(for pkg in $pip_packages; do printf ' %s' $pkg; done)
     fi 
 
     for bin in $bin_export 
@@ -81,21 +74,39 @@ dbox_install_single() {
 }
 
 
-dbox_install_all() {
+# First argument is path to packages.yaml to use
+dbox_install_all_from_yaml() {
+    [ $# -ne 1 ] && echo "$0 <packages.yaml>" && exit 1
+    local packages_yaml="$1"
+
     i=0
-    local dbox_count=$(get_yaml_distrobox_length)
+    local dbox_count=$(get_yaml_distrobox_length $packages_yaml)
 
     while [ $i -lt $dbox_count ]
     do 
         echo "$i"
         local key=".distrobox[${i}]"
-        local name=$(yq "${key}.name" < $PACKAGES_FILE)
-        local image=$(yq "${key}.image" < $PACKAGES_FILE)
-        distrobox create --yes -i "${image}" "${name}"
-        [ $? -ne 0 ] && echo "distrobox create --yes -i ${image} ${name} failed" && exit 1
-        distrobox enter "${name}" -- bash -c "source ./packages.sh && dbox_install_single $i"
+        local name=$(yq "${key}.name" < $packages_yaml)
+        local image=$(yq "${key}.image" < $packages_yaml)
+
+        # Check for an empty line (new-line). If no image is specified
+        if [ 0 -eq $(container_exists "${name}") ]
+        then 
+            distrobox create --yes -i "${image}" "${name}"
+
+            # If it failed to create, critically fail
+            [ 0 -ne $? ] && echo "distrobox create --yes -i ${image} ${name} failed" && exit 1
+        fi
+
+        # Run the single installer in the new dbox
+        distrobox enter "${name}" -- bash -c "source ./src/packages.sh && dbox_install_single ${packages_yaml} $i"
         (( i++ ))
     done
 }
 
+
+dbox_install_all() {
+    dbox_install_all_from_yaml $PACKAGES_FILE
+    for f in $PACKAGES_CUSTOM_FMT; do dbox_install_all_from_yaml $f; done
+}
 
