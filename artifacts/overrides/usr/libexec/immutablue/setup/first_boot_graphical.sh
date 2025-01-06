a0dca54699709a2c577a7d55554edd829ef3543c
Zach Podbielniak <z.podbielniak@disroot.org>
2025-01-06 22:13:07 +0000

    Resolve "bug(installer): first-boot and ptyxis"

--- artifacts/overrides/usr/libexec/immutablue/setup/first_boot_graphical.sh
+++ artifacts/overrides/usr/libexec/immutablue/setup/first_boot_graphical.sh
@@ -8,6 +8,7 @@
 # distrobox installs as well as post_install.sh
 set -euxo pipefail
 source /usr/libexec/immutablue/immutablue-header.sh
+TERMINAL_CMD=$(immutablue_get_terminal_command)
 
 if [[ -f /etc/immutablue/setup/did_first_boot_graphical ]]
 then 
@@ -21,23 +22,23 @@ if [[ "$(immutablue_has_internet)" == "${FALSE}" ]]
 then
     # If nuclues we can't show a graphical indicator
     # If not we can show a terminal with the message
-    if [[ "$(immutablue_build_is_nucleus)" == "${TRUE}" ]]
+    if [[ "$(immutablue_build_is_nucleus)" == "${TRUE}" ]] || [[ "${TERMINAL_CMD}" == "" ]]
     then 
         echo "Please connect to the internet first and restart."
         exit 1
     else 
-        ptyxis -- bash -c 'echo "Please connect to the internet and then restart to finish installation." && read -p "hit inter to continure, reboot when ready (after setting up internet)"'
+        ${TERMINAL_CMD} bash -c 'echo "Please connect to the internet and then restart to finish installation." && read -p "hit inter to continure, reboot when ready (after setting up internet)"'
         exit 1
     fi
 fi
 
 
 # Check if this is a nucleus build, if so, this can't be done in a graphical install
-if [[ "$(immutablue_build_is_nucleus)" == "${TRUE}" ]]
+if [[ "$(immutablue_build_is_nucleus)" == "${TRUE}" ]] || [[ "${TERMINAL_CMD}" == "" ]]
 then
     immutablue install 
 else 
-    ptyxis -- bash -c 'immutablue install && echo "please reboot one last time" && read -p "hit enter to continue. reboot when ready"'
+    ${TERMINAL_CMD} bash -c 'immutablue install && echo "please reboot one last time" && read -p "hit enter to continue. reboot when ready"'
 fi
 
 echo "Please reboot one last time"
