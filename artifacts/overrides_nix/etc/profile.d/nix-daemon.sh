4f99f1d3fe4fddc7c62542ed5d3e86ab5627cc00
Zach Podbielniak <z.podbielniak@disroot.org>
2025-10-16 21:18:05 -0400

    bug(nix): fix bug with nix-daemon.sh profile script

--- artifacts/overrides_nix/etc/profile.d/nix-daemon.sh
+++ artifacts/overrides_nix/etc/profile.d/nix-daemon.sh
@@ -1,3 +1,7 @@
 # Add Nix to the system profile
 # this will put nix in the $PATH
-. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]
+then
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+fi
+
