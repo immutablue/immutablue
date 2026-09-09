62296ce910793df74358553d2384186bc410d8cb
Zach Podbielniak <z.podbielniak@disroot.org>
2026-09-09 00:01:53 -0400

    fix(snapshot): reject directory bind mounts before restore

--- makefiles/50-tests.mk
+++ makefiles/50-tests.mk
@@ -20,6 +20,7 @@
 tests: test
 
 test_regressions:
+	bash tests/test_snapshot_restore_guard.sh
 	bash tests/test_boot_hooks.sh
 	bash tests/test_build_image_config.sh
 	bash tests/test_latest_tag.sh
